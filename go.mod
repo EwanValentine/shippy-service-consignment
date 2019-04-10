bcfe5df7a5a895211122b79bb06852a676c6e129
Ewan Valentine <ewan.valentine@peak.ai>
2019-04-10 22:32:15 +0100

    ADDED get consignments endpoint

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/EwanValentine/shippy-service-consignment
 
 go 1.12
 
-require google.golang.org/grpc v1.20.0
+require (
+	github.com/golang/protobuf v1.2.0
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+	google.golang.org/grpc v1.20.0
+)
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net"
 	"sync"
 
-	pb "github.com/shippy/consignment-service/proto/consignment"
+	pb "github.com/EwanValentine/shippy-service-consignment/proto/consignment"
 	"google.golang.org/grpc"
 )
 
@@ -16,6 +16,7 @@ const (
 
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 // Repository - Dummy repository, this simulates the use of a datastore
@@ -32,6 +33,11 @@ func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, er
 	repo.consignments = updated
 	repo.mu.Unlock()
 	return consignment, nil
+}
+
+// GetAll consignments
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
 }
 
 // Service should implement all of the methods to satisfy the service
@@ -56,6 +62,12 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 	// Return matching the `Response` message we created in our
 	// protobuf definition.
 	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+// GetConsignments -
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
 }
 
 func main() {
